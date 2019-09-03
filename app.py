92161de983317e675d31ae8d80dce6bc8c3a74df
Akanksha <akanksha0990@gmail.com>
2019-09-03 19:03:16 -0400

    changes in app.py n html file

--- app.py
+++ app.py
@@ -14,15 +14,15 @@ app.secret_key = 'password'
 def index():
     return render_template('upload.html')
 
-
-# Upload the file
 @app.route('/uploader', methods = ['GET','POST'])
 def upload_file():
     if request.method == 'POST':
         f = request.files['file']
+        # Upload the file
         if (f.filename == 'walk.cc'):
             f.save(secure_filename(f.filename))
             flash('File uploaded successfully!')
+            # Compile and Execute the code
             subprocess.call("rm -f ./a.out", shell=True)
             retcode = subprocess.call("/usr/bin/g++ walk.cc", shell=True)
             if retcode:
@@ -49,33 +49,7 @@ def upload_file():
             flash('No file chosen')
             return render_template('upload.html')
 
-
-# Compile and Execute the code
-@app.route('/result', methods = ['GET','POST'])
-def cande_file():
-    if request.method == 'POST':
-        subprocess.call("rm -f ./a.out", shell=True)
-        retcode = subprocess.call("/usr/bin/g++ walk.cc", shell=True)
-        if retcode:
-            print("failed to compile walk.cc")
-            exit
-            flash("Failed to compile walk.cc!")
-            flash ("Upload the file again!")
-            return render_template('upload.html')
-
-        subprocess.call("rm -f ./output", shell=True)
-        retcode = subprocess.call("./test.sh", shell=True)
-        message = "Score: " + str(retcode) + " out of 2 correct."
-        print (message)
-        print("*************Original submission*************")
-        with open('walk.cc','r') as fs:
-            print(fs.read())
-            flash(message)
-            flash(fs.read())
-        return render_template('result.html')
     
-
-
 
 if __name__ ==  '__main__':
     app.run(host="0.0.0.0", debug=True)
