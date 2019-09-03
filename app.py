0466e4fa4440f8ed3b85dcf1d5c4e5c1320be198
Akanksha <akanksha0990@gmail.com>
2019-09-03 18:53:05 -0400

    changes in app.py

--- app.py
+++ app.py
@@ -20,10 +20,31 @@ def index():
 def upload_file():
     if request.method == 'POST':
         f = request.files['file']
-        if (f):
+        if (f.filename == 'walk.cc'):
             f.save(secure_filename(f.filename))
             flash('File uploaded successfully!')
-            return render_template('cande.html')
+            subprocess.call("rm -f ./a.out", shell=True)
+            retcode = subprocess.call("/usr/bin/g++ walk.cc", shell=True)
+            if retcode:
+                print("failed to compile walk.cc")
+                exit
+                flash("Failed to compile walk.cc!")
+                flash ("Upload the file again!")
+                return render_template('upload.html')
+
+            subprocess.call("rm -f ./output", shell=True)
+            retcode = subprocess.call("./test.sh", shell=True)
+            message = "Score: " + str(retcode) + " out of 2 correct."
+            print (message)
+            print("*************Original submission*************")
+            with open('walk.cc','r') as fs:
+                print(fs.read())
+                flash(message)
+                flash(fs.read())
+            return render_template('result.html')
+        elif (f.filename != 'walk.cc'):
+            flash('Please upload walk.cc file only!')
+            return render_template('upload.html')
         else :
             flash('No file chosen')
             return render_template('upload.html')
@@ -49,7 +70,8 @@ def cande_file():
         print("*************Original submission*************")
         with open('walk.cc','r') as fs:
             print(fs.read())
-            flash(str(retcode))
+            flash(message)
+            flash(fs.read())
         return render_template('result.html')
     
 
